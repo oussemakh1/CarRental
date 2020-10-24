2909079fccbffcad879f9ac6e9011071b694b80e
Oussema Khlifi <46871935+oussemakh1@users.noreply.github.com>
2020-10-24 10:04:27 +0200

    Update navbar.inc.php

--- View3/includes/navbar.inc.php
+++ View3/includes/navbar.inc.php
@@ -15,7 +15,7 @@ if($Session::CheckAdminLogin() == true){
 <!-- ============================================================== -->
 <div class="dashboard-header">
     <nav class="navbar navbar-expand-lg bg-white fixed-top">
-        <a class="navbar-brand" href="index.html"><img class="logo-img" src="../assets/images/logo.png" alt="logo"></a>
+        <a class="navbar-brand" href="#"><img class="logo-img" src="../assets/images/logo.png" alt="logo"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
