2e65a96386de70f8a3a8b6ed210f44b3e9aa3eb7
Oussema Khlifi <46871935+oussemakh1@users.noreply.github.com>
2020-10-26 11:46:59 +0100

    Update facture.php

--- View3/Facture/facture.php
+++ View3/Facture/facture.php
@@ -10,6 +10,9 @@
      $taken_data = $_GET['data];
      $data = unserialize($taken_data);
      $company_data = $factureController->CompanyInfo();
+     $factureController->insert_facture($data);
+
+     
   } 
     elseif(isset($_GET['data_update']))
   {
